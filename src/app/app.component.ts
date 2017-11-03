2107cfc403d5a34bb8ffd5e71af4f4442d1a985f
mfullford <marissafullford@gmail.com>
2017-11-03 12:13:28 -0600

    First half done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
+      {phrase: 'First comment!',
+      author: 'Penelope'
+  		},
+      {phrase: 'Nice work!',
+      author: 'Lancelot'
+		},
+      {phrase: 'I would also like to congratulate you!',
+      author: 'Arthur'
+  	}
   ];
+
+  editValue= true;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  addComment(newPhrase, newAuthor) {
+  		let newComment = {
+  			phrase : newPhrase, 
+  			author : newAuthor};
+  		console.log(newComment)
+  		this.comments.push(newComment);
+  }
+
+  deleteComment(comment) {
+  		for (let i=1; i<this.comments.length; i++) {
+  			if (this.comments[i].phrase === comment.phrase)
+  			this.comments.splice(i, 1)
+  		}
+  }
+
+  editComment() {
+  		this.editValue = null;
+  		console.log(this.editValue);
+  }
 }
